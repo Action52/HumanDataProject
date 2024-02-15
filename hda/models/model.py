d05e1f928cc0f14798765387f99b30e39f947e01
Luis Alfredo Leon Villapun <luisalfredoleonvillapun@MacBook-Air-de-Luis.local>
2024-02-15 13:33:36 +0100

    Latest changes conv layer

--- hda/models/model.py
+++ hda/models/model.py
@@ -2,7 +2,9 @@
 
 import tensorflow as tf
 
-from keras.layers import Layer, Flatten, Conv1D, Dense, Lambda,  Dropout, GRU, SimpleRNN, LSTM
+from keras.layers import Layer, Flatten, Conv1D, Dense, Lambda,  Dropout, GRU, SimpleRNN, LSTM, Reshape, MaxPooling1D
+from ncps.tf import LTC, CfC
+from ncps.wirings import NCP, AutoNCP
 from sklearn.utils.class_weight import compute_class_weight
 from hda.models.base_model import WandbKerasModel
 
@@ -12,9 +14,9 @@ from hda.utils import load_config, get_dataset_shape, predict_and_plot
 import numpy as np
 
 
-class WindowsConvolutionLayer(Layer):
+class SimpleWindowsConvolutionLayer(Layer):
     def __init__(self, num_diodes, convolutions_conf, **kwargs):
-        super(WindowsConvolutionLayer, self).__init__(**kwargs)
+        super(SimpleWindowsConvolutionLayer, self).__init__(**kwargs)
         self.num_diodes = num_diodes  # Number of diodes/channels
         self.conv_blocks = [
             [Conv1D(**conv_conf) for _, conv_conf in convolutions_conf.items()]
@@ -35,6 +37,43 @@ class WindowsConvolutionLayer(Layer):
         combined = Lambda(lambda x: tf.concat(x, axis=-2))(conv_outputs)  # Shape: (20, 6, 207, 22, 4)
         return combined
 
+
+class WindowsConvolutionLayer(Layer):
+    def __init__(self, num_diodes, convolutions_conf, **kwargs):
+        super(WindowsConvolutionLayer, self).__init__(**kwargs)
+        self.num_diodes = num_diodes  # Number of diodes/channels
+        self.conv_blocks = [
+            [Conv1D(**conv_conf) for _, conv_conf in convolutions_conf.items()]
+            for _ in range(num_diodes)
+        ]
+        self.max_pool = MaxPooling1D(padding='same', data_format="channels_first")
+
+    def call(self, inputs):
+        conv_outputs = []
+        for i in range(self.num_diodes):
+            diode_slice = Lambda(lambda x: x[:, :, :, i:i + 1])(
+                inputs)  # Shape: (batch_size, versions, steps, 1)
+            for conv_layer in self.conv_blocks[i]:
+                diode_slice = conv_layer(
+                    diode_slice)  # Apply convolution to each diode slice
+
+            # Reshape to combine versions and steps for max pooling
+            # New shape: (batch_size, versions*steps, features)
+            shape_before_pooling = tf.shape(diode_slice)
+            diode_slice = Lambda(lambda x: tf.reshape(x, [-1,shape_before_pooling[1] * shape_before_pooling[3],shape_before_pooling[2]]))(diode_slice)
+            # Apply max pooling
+            diode_slice = self.max_pool(diode_slice)
+            # Restore original shape: (batch_size, versions, steps, features)
+            diode_slice = Lambda(lambda x: tf.reshape(x, [-1, shape_before_pooling[1], diode_slice.shape[2], shape_before_pooling[3]]))(diode_slice)
+            # Reshape to add an extra dimension for diode feature maps
+            # Shape: (batch_size, versions, steps, 1, features)
+            diode_slice = Lambda(lambda x: tf.expand_dims(x, -2))(diode_slice)
+            conv_outputs.append(diode_slice)
+
+        # Stack along the new dimension to keep diodes and their feature maps separate
+        combined = Lambda(lambda x: tf.concat(x, axis=-2))(conv_outputs)  # Shape: (batch_size, versions, steps, num_diodes, features)
+        #print(combined.shape)
+        return combined
 
 class DenseLayer(Layer):
     def __init__(self, dense_conf, **kwargs):
@@ -117,6 +156,39 @@ class TimeSeriesModelLSTM(tf.keras.Model):
         return x
 
 
+class TimeSeriesModelCfC(tf.keras.Model):
+    def __init__(self, diodes,  convolutions_conf, dense_conf, cfc_conf, **kwargs):
+        super(TimeSeriesModelCfC, self).__init__(**kwargs)
+        self.multi_version_conv = WindowsConvolutionLayer(diodes, convolutions_conf)
+        self.cfc_layer = CfC(**cfc_conf)
+        self.dense = DenseLayer(dense_conf)
+
+    def call(self, inputs):
+        x = self.multi_version_conv(inputs)
+        x = tf.reshape(x, shape=(-1, x.shape[2], x.shape[1] * x.shape[3] * x.shape[4]))
+        x = self.cfc_layer(x)
+        x = self.dense(x)
+        return x
+
+
+class TimeSeriesModelCfCWithNCP(tf.keras.Model):
+    def __init__(self, diodes,  convolutions_conf, dense_conf, cfc_conf, wiring_conf,**kwargs):
+        super(TimeSeriesModelCfCWithNCP, self).__init__(**kwargs)
+        self.multi_version_conv = WindowsConvolutionLayer(diodes, convolutions_conf)
+        self.wiring = AutoNCP(**wiring_conf)
+        if 'units' in cfc_conf:
+            del cfc_conf['units']
+        self.cfc_layer = CfC(self.wiring, **cfc_conf)
+        self.dense = DenseLayer(dense_conf)
+
+    def call(self, inputs):
+        x = self.multi_version_conv(inputs)
+        x = tf.reshape(x, shape=(-1, x.shape[2], x.shape[1] * x.shape[3] * x.shape[4]))
+        x = self.cfc_layer(x)
+        x = self.dense(x)
+        return x
+
+
 def main():
     config = load_config("config.yaml")
     batch_size, versions, time_steps, diodes = get_dataset_shape(config)
@@ -142,7 +214,9 @@ def main():
         "simple": TimeSeriesModelSimple(diodes, config['convolutions_conf'], config['dense_conf']),
         "gru": TimeSeriesModelGRU(diodes, config['convolutions_conf'], config['dense_conf'], gru_conf=config['gru']),
         "vanilla_rnn": TimeSeriesModelVanillaRNN(diodes, config['convolutions_conf'], config['dense_conf'], rnn_conf=config['simple_rnn']),
-        "lstm": TimeSeriesModelLSTM(diodes, config['convolutions_conf'], config['dense_conf'], lstm_conf=config['lstm'])
+        "lstm": TimeSeriesModelLSTM(diodes, config['convolutions_conf'], config['dense_conf'], lstm_conf=config['lstm']),
+        "cfc": TimeSeriesModelCfC(diodes, config['convolutions_conf'], config['dense_conf'], cfc_conf=config['cfc']),
+        "cfc_ncp": TimeSeriesModelCfCWithNCP(diodes, config['convolutions_conf'], config['dense_conf'], cfc_conf=config['cfc'], wiring_conf=config['wiring'])
     }
     
     for name, model in experiments.items():
@@ -153,7 +227,7 @@ def main():
 
         # Wrap the model with WandbKerasModel
         wandb_model = WandbKerasModel(
-            model=model, project_name="hda-sat", config={}, entity="bdma"
+            model=model, project_name="hda-luis-test-new", config={}, entity="bdma"
         )
         # Model summary to check the architecture
         wandb_model.model.summary()
